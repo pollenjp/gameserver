a10191099281611163b08db244fa42f5abb2523f
pollenJP <polleninjp@gmail.com>
2021-12-28 07:32:52 +0000

    Update api (/room/create and /room/list)

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security.http import HTTPAuthorizationCredentials, HTTPBearer
@@ -6,6 +7,7 @@ from pydantic import BaseModel
 
 from . import model
 from .model import SafeUser
+from .room_model import RoomInfo, RoomUser, create_room, get_rooms_by_live_id, LiveDifficulty, join_room
 
 app = FastAPI()
 
@@ -65,3 +67,39 @@ def update(req: UserCreateRequest, token: str = Depends(get_auth_token)):
     # print(req)
     model.update_user(token, req.user_name, req.leader_card_id)
     return {}
+
+
+class RoomCreateRequest(BaseModel):
+    live_id: int
+    select_difficulty: LiveDifficulty
+
+
+class RoomCreateResponse(BaseModel):
+    room_id: int
+
+
+@app.post("/room/create", response_model=RoomCreateResponse)
+def room_create(req: RoomCreateRequest, token: str = Depends(get_auth_token)):
+    room_id: int = create_room(req.live_id)
+    user: SafeUser = model.get_user_by_token(token)
+    join_room(room_id=room_id, user_id=user.id,
+              live_difficulty=req.select_difficulty, is_host=True)
+    print(f"create room: {room_id=}")
+    return RoomCreateResponse(room_id=room_id)
+
+
+class RoomListRequest(BaseModel):
+    live_id: int
+
+
+class RoomListResponse(BaseModel):
+    room_info_list: List[RoomInfo]
+
+
+@app.post("/room/list", response_model=List[RoomInfo])
+def room_list(req: RoomListRequest):
+    """Update user attributes"""
+    rooms: List[RoomInfo] = get_rooms_by_live_id(req.live_id)
+    print(f"{rooms=}")
+    print(f"{type(rooms)=}")
+    return RoomListResponse(room_info_list=rooms)
