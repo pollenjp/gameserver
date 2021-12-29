75a57e6f6b3f8e08f3f97de5b8e3dabd1c2087da
pollenjp <polleninjp@gmail.com>
2021-12-29 22:41:49 +0900

    refactoring test_room

--- tests/test_room.py
+++ tests/test_room.py
@@ -1,5 +1,7 @@
+# Third Party Library
 from fastapi.testclient import TestClient
 
+# First Party Library
 from app.api import app
 
 client = TestClient(app)
@@ -24,9 +26,7 @@ def _auth_header(i=0):
 
 
 def test_room_1():
-    response = client.post(
-        "/room/create", headers=_auth_header(), json={"live_id": 1001, "select_difficulty": 1}
-    )
+    response = client.post("/room/create", headers=_auth_header(), json={"live_id": 1001, "select_difficulty": 1})
     assert response.status_code == 200
 
     room_id = response.json()["room_id"]
@@ -36,23 +36,18 @@ def test_room_1():
     assert response.status_code == 200
     print("room/list response:", response.json())
 
-    response = client.post(
-        "/room/wait", headers=_auth_header(), json={"room_id": room_id}
-    )
+    response = client.post("/room/wait", headers=_auth_header(), json={"room_id": room_id})
     assert response.status_code == 200
     print("room/wait response:", response.json())
 
-    response = client.post(
-        "/room/start", headers=_auth_header(), json={"room_id": room_id}
-    )
+    response = client.post("/room/start", headers=_auth_header(), json={"room_id": room_id})
     assert response.status_code == 200
     print("room/wait response:", response.json())
 
     response = client.post(
         "/room/end",
         headers=_auth_header(),
-        json={"room_id": room_id, "score": 1234,
-              "judge_count_list": [4, 3, 2]},
+        json={"room_id": room_id, "score": 1234, "judge_count_list": [4, 3, 2]},
     )
     assert response.status_code == 200
     print("room/end response:", response.json())
@@ -65,32 +60,32 @@ def test_room_1():
     print("room/end response:", response.json())
 
 
-class TestRoom:
-    def setup_class(self):
-        response = client.post(
-            "/room/create", headers=_auth_header(), json={"live_id": 1001, "select_difficulty": 1, }
-        )
-        assert response.status_code == 200
+# class TestRoom:
+#     def setup_class(self):
+#         response = client.post(
+#             "/room/create", headers=_auth_header(), json={"live_id": 1001, "select_difficulty": 1, }
+#         )
+#         assert response.status_code == 200
 
-    def teardown_class(self):
-        pass
+#     def teardown_class(self):
+#         pass
 
-    def setup_method(self, method):
-        pass
+#     def setup_method(self, method):
+#         pass
 
-    def teardown_method(self, method):
-        pass
+#     def teardown_method(self, method):
+#         pass
 
-    @pytest.mark.parametrize(
-        "info_dict",
-        [
-            get_correct_info_dict(),
-            pytest.param(
-                get_incorrect_info_dict_datetime(
-                    info_dict=get_correct_info_dict()),
-                marks=pytest.mark.xfail(strict=True),
-            ),
-        ],
-    )
-    def test_hogehoge():
-        hogehoge()
+#     @pytest.mark.parametrize(
+#         "info_dict",
+#         [
+#             get_correct_info_dict(),
+#             pytest.param(
+#                 get_incorrect_info_dict_datetime(
+#                     info_dict=get_correct_info_dict()),
+#                 marks=pytest.mark.xfail(strict=True),
+#             ),
+#         ],
+#     )
+#     def test_hogehoge():
+#         hogehoge()
