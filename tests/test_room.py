abc067f11fcbd3daa45b046bafd5c41e31de0d8f
pollenjp <polleninjp@gmail.com>
2021-12-30 05:19:10 +0900

    Update test_room.py

--- tests/test_room.py
+++ tests/test_room.py
@@ -1,7 +1,11 @@
 # Standard Library
 from logging import getLogger
+from typing import Any
+from typing import Dict
+from typing import List
 
 # Third Party Library
+import pytest
 from fastapi.testclient import TestClient
 
 # First Party Library
@@ -11,106 +15,120 @@ from app.api import app
 logger = getLogger(__name__)
 
 client = TestClient(app)
-user_tokens = []
 
 
-def _create_users():
+def _create_users(num: int = 10) -> List[str]:
+    user_tokens = []
     for i in range(10):
         response = client.post(
             "/user/create",
             json={"user_name": f"room_user_{i}", "leader_card_id": 1000},
         )
         user_tokens.append(response.json()["user_token"])
+    return user_tokens
 
 
-_create_users()
-
-
-def _auth_header(i=0):
-    token = user_tokens[i]
+def _get_auth_header(token: str) -> Dict[str, str]:
     return {"Authorization": f"bearer {token}"}
 
 
-def test_room_1():
-    response = client.post(
-        "/room/create",
-        headers=_auth_header(),
-        json={
-            "live_id": 1001,
-            "select_difficulty": room_model.LiveDifficulty.normal,
-        },
+class TestRoom:
+    def setup_class(self):
+        self.user_tokens: List[str] = _create_users()
+        self.auth_header = _get_auth_header(self.user_tokens[0])
+
+    def teardown_class(self):
+        pass
+
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+    @pytest.mark.parametrize(
+        "room_arg",
+        [
+            pytest.param(
+                dict(
+                    live_id=1,
+                    select_difficulty=int(room_model.LiveDifficulty.normal),
+                ),
+            ),
+            pytest.param(
+                dict(
+                    live_id=2,
+                    select_difficulty=int(room_model.LiveDifficulty.hard),
+                ),
+            ),
+            pytest.param(
+                dict(
+                    live_id=2,
+                    select_difficulty=3,  # 3 does not exists
+                ),
+                marks=pytest.mark.xfail(strict=True),
+            ),
+        ],
     )
-    assert response.status_code == 200
+    def test_create_room(self, room_arg: Dict[str, Any]):
+        response = client.post(
+            "/room/create",
+            headers=self.auth_header,
+            json=dict(
+                live_id=room_arg["live_id"],
+                select_difficulty=int(room_arg["select_difficulty"]),
+            ),
+        )
+        assert response.status_code == 200
 
-    room_id = response.json()["room_id"]
-    logger.info(f"room/create {room_id=}")
+    def test_start_to_end(self):
+        response = client.post(
+            "/room/create",
+            headers=self.auth_header,
+            json=dict(
+                live_id=1001,
+                select_difficulty=int(room_model.LiveDifficulty.normal),
+            ),
+        )
+        assert response.status_code == 200
+        room_id = response.json()["room_id"]
+        logger.info(f"room/create {room_id=}")
 
-    response = client.post("/room/list", json={"live_id": 1001})
-    assert response.status_code == 200
-    logger.info("room/list response:", response.json())
+        response = client.post("/room/list", json=dict(live_id=1001))
+        assert response.status_code == 200
+        logger.info("room/list response:", response.json())
 
-    response = client.post(
-        "/room/join",
-        headers=_auth_header(),
-        json={
-            "room_id": room_id,
-            "select_difficulty": room_model.LiveDifficulty.hard,
-        },
-    )
-    assert response.status_code == 200
-    logger.info("room/join response:", response.json())
-    assert response.json()["join_room_result"] in [result for result in room_model.JoinRoomResult]
+        response = client.post(
+            "/room/join",
+            headers=self.auth_header,
+            json={
+                "room_id": room_id,
+                "select_difficulty": room_model.LiveDifficulty.hard,
+            },
+        )
+        assert response.status_code == 200
+        logger.info("room/join response:", response.json())
+        assert response.json()["join_room_result"] in [result for result in room_model.JoinRoomResult]
 
-    response = client.post("/room/wait", headers=_auth_header(), json={"room_id": room_id})
-    assert response.status_code == 200
-    logger.info("room/wait response:", response.json())
+        response = client.post("/room/wait", headers=self.auth_header, json={"room_id": room_id})
+        assert response.status_code == 200
+        logger.info("room/wait response:", response.json())
 
-    response = client.post("/room/start", headers=_auth_header(), json={"room_id": room_id})
-    assert response.status_code == 200
-    logger.info("room/wait response:", response.json())
+        response = client.post("/room/start", headers=self.auth_header, json={"room_id": room_id})
+        assert response.status_code == 200
+        logger.info("room/wait response:", response.json())
 
-    response = client.post(
-        "/room/end",
-        headers=_auth_header(),
-        json={"room_id": room_id, "score": 1234, "judge_count_list": [4, 3, 2]},
-    )
-    assert response.status_code == 200
-    logger.info("room/end response:", response.json())
+        response = client.post(
+            "/room/end",
+            headers=self.auth_header,
+            json={"room_id": room_id, "score": 1234, "judge_count_list": [4, 3, 2]},
+        )
+        assert response.status_code == 200
+        logger.info("room/end response:", response.json())
 
-    response = client.post(
-        "/room/result",
-        json={"room_id": room_id},
-    )
-    assert response.status_code == 200
-    logger.info("room/end response:", response.json())
-
-
-# class TestRoom:
-#     def setup_class(self):
-#         response = client.post(
-#             "/room/create", headers=_auth_header(), json={"live_id": 1001, "select_difficulty": 1, }
-#         )
-#         assert response.status_code == 200
-
-#     def teardown_class(self):
-#         pass
-
-#     def setup_method(self, method):
-#         pass
-
-#     def teardown_method(self, method):
-#         pass
-
-#     @pytest.mark.parametrize(
-#         "info_dict",
-#         [
-#             get_correct_info_dict(),
-#             pytest.param(
-#                 get_incorrect_info_dict_datetime(
-#                     info_dict=get_correct_info_dict()),
-#                 marks=pytest.mark.xfail(strict=True),
-#             ),
-#         ],
-#     )
-#     def test_hogehoge():
-#         hogehoge()
+        response = client.post(
+            "/room/result",
+            json={"room_id": room_id},
+        )
+        assert response.status_code == 200
+        logger.info("room/end response:", response.json())
